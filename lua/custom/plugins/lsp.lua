5d59788949c152b5252c7b1d6f8e09fea28597fa
Dan Chevalier <chevalier.dan@gmail.com>
2024-12-05 09:00:10 -0500

    Add typescript server and tmux

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -214,6 +214,7 @@ return { -- LSP Configuration & Plugins
       -- But for many setups, the LSP (`tsserver`) will work just fine
       -- tsserver = {},
       --
+      ts_ls = {},
 
       lua_ls = {
         -- cmd = {...},
